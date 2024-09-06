5cca8ec94ce98f836b3fdec594bfbbb8d15921d4
Cristi-Fogel <37100222+Cristi-Fogel@users.noreply.github.com>
2024-09-06 07:37:40 +0300

    eachCell

--- excelDemo.js
+++ excelDemo.js
@@ -1,1 +1,11 @@
 const ExcelJs = require('excel.js');
+
+const workbook = new ExcelJs.Workbook();
+const worksheet = workbook.getWorksheet('Sheet1');
+worksheet.eachRow((row, rowNumber) =>
+    {
+        row.eachCell((cell, colNumber)=>
+            {
+                console.log(cell.value);
+            })
+    })
