003705be32fc1d9aeb100cbe8d4b6662aa5da11d
Cristi-Fogel <37100222+Cristi-Fogel@users.noreply.github.com>
2024-09-10 08:24:50 +0300

    add file and brute force upload-changes based on column identifier

--- excelDemo.js
+++ excelDemo.js
@@ -1,11 +1,36 @@
-const ExcelJs = require('excel.js');
+const ExcelJs = require('exceljs');
 
-const workbook = new ExcelJs.Workbook();
-const worksheet = workbook.getWorksheet('Sheet1');
-worksheet.eachRow((row, rowNumber) =>
-    {
-        row.eachCell((cell, colNumber)=>
-            {
-                console.log(cell.value);
-            })
-    })
+
+async function excelTest(){
+
+    let output = {row: -1, column: -1};
+
+    const workbook = new ExcelJs.Workbook();
+    await workbook.xlsx.readFile("./files/excellDownloadTest.xlsx");
+
+    const worksheet = workbook.getWorksheet('Sheet1');
+    worksheet.eachRow((row, rowNumber) =>
+        {
+            row.eachCell((cell, colNumber)=>
+                {
+                    // // print all excel content
+                    // console.log(cell.value);
+                    
+                    // //identify item
+                    if(cell.value === "Apple"){
+                        console.log(rowNumber) //3
+                        console.log(colNumber) //2
+
+                        output.row = rowNumber;
+                        output.column = colNumber;
+                    }
+                })
+        })
+
+    const cell = worksheet.getCell(output.row, output.column);
+    cell.value = "Iphone";
+    await workbook.xlsx.writeFile("./files/excellDownloadTest.xlsx");
+
+}
+
+excelTest();
